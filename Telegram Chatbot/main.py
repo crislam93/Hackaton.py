b91ce817764762e7dcfa083494732d056633b690
crislam93 <39211349+crislam93@users.noreply.github.com>
2020-05-16 13:44:22 +0200

    Telegram chatbot

---
+++ Telegram Chatbot/main.py
@@ -0,0 +1,34 @@
+from telegram.ext import Updater, CommandHandler
+
+def main():
+    #instanciamos el Updater
+    updater = Updater(toke=open("/token.txt").read(), use_context= True)
+    
+    #añadimos un manejador al comando start de Telegram
+    updater.dispatcher.add_handler(CommandHandler("start", start))
+
+    #añadimos un manejador para el comando repite de Telegram
+    updater.dispatcher.add_handler(CommandHandler("repite", repite))
+
+    #añadir manejador para el comando suma de Telegram
+    updater.dispatcher.add_handler(CommandHandler("suma", suma))
+
+    #empezamos a pedir notificaciones a Telegram
+    updater.start_polling()
+
+    #Capturamos señales de parada
+    updater.idle()
+
+
+def start(update, context):
+    update.message.reply_text("Hola, soy un Bot")  
+
+def repite(update, context):
+    update.message.reply_text(update.message.text)
+
+def suma(update, context):
+    #args = [2, 2]
+    resultado = int(context.args[0]) + int(context.args[1])
+    update.message.reply_text("El resultado es " + str(resultado))
+
+main()
